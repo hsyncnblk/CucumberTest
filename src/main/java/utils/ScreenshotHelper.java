598c28a9f7a5e2c7506e4644830394e09f2fc6b8
huseyin cinibulak <huseyincnblk@gmail.com>
2025-01-22 14:23:29 +0300

    LogClass , screenshotHelper and WebDriverFactory added

---
+++ src/main/java/utils/ScreenshotHelper.java
@@ -0,0 +1,30 @@
+package utils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ScreenshotHelper {
+
+    public static void takeScreenshot(WebDriver driver, String fileName) {
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String screenshotPath = "src/test/resources/screenshots/" + fileName + "_" + timestamp + ".png";
+
+        try {
+            FileHandler.copy(screenshot, new File(screenshotPath));
+            System.out.println("Screenshot saved successfully: " + screenshotPath);
+        } catch (Exception e) {
+            System.out.println("Failed to save screenshot: " + e.getMessage());
+        }
+    }
+
+
+}
---
+++ src/main/java/utils/WebDriverFactory.java
@@ -0,0 +1,37 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverFactory {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+
+            Log.info("WebDriver instance is being created...");
+
+            ChromeOptions cop = new ChromeOptions();
+
+            cop.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(cop);
+
+            driver.manage().window().maximize();
+
+            Log.info("WebDriver instance created and window maximized.");
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            Log.info("Closing WebDriver instance...");
+            driver.quit();
+            driver = null;
+            Log.info("WebDriver instance closed successfully.");
+        }
+    }
+
+}
